0279e4f87b80d4abd5baed068bfbc94314354349
Charlie Taft <taft.charlie@gmail.com>
2022-05-10 15:19:00 -0500

    Update Program.cs

--- CreatorClasses-Experience-API/Program.cs
+++ CreatorClasses-Experience-API/Program.cs
@@ -59,11 +59,10 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
